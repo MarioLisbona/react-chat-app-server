f3271518943d26c8ad510f5aacf083c5421c7f84
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-26 09:31:12 +1000

    remove nested emit on message

--- index.js
+++ index.js
@@ -14,21 +14,16 @@ let users = [];
 
 socketIO.on("connection", (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
+
+  //sends the message to all the users on the server
   socket.on("message", (data) => {
+    console.log("logging messge ---->", data);
     socketIO.emit("messageResponse", data);
-  });
-
-  socket.on("typing", (data) => socket.broadcast.emit("typingResponse", data));
-
-  socket.on("newUser", (data) => {
-    users.push(data);
-    socketIO.emit("newUserResponse", users);
   });
 
   socket.on("disconnect", () => {
     console.log("🔥: A user disconnected");
-    users = users.filter((user) => user.socketID !== socket.id);
-    socketIO.emit("newUserResponse", users);
+
     socket.disconnect();
   });
 });
